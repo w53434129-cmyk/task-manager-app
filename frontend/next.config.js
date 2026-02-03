564b49b9f7281b0aeebd8e8d41c13435e7bc1db9
w53434129-cmyk <w53434129@gmail.com>
2026-02-03 16:50:03 +0500

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
+
+  // Serve static assets from the public URL
+  assetPrefix: isProd ? 'http://localhost:30080' : '',
 };
 
 module.exports = nextConfig;
