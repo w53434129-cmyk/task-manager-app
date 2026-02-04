cfef2f1a06501965968137452873ab8fe463e2a9
w53434129-cmyk <w53434129@gmail.com>
2026-02-04 15:55:08 +0500

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -27,3 +27,26 @@ export const createTask = async (task, token) => {
   });
   return res.json();
 };
+
+export const updateTask = async (id, task, token) => {
+  const res = await fetch(`${API_URL}/api/tasks/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(task),
+  });
+  return res.json();
+};
+
+export const deleteTask = async (id, token) => {
+  const res = await fetch(`${API_URL}/api/tasks/${id}`, {
+    method: "DELETE",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return res.json();
+};
+
